aadb25a187acc213fe70419dc71c4c7b68c73df5
Samuel Clay <samuel@ofbrooklyn.com>
2018-09-16 16:09:58 -0400

    Using template.

--- hw2.py
+++ hw2.py
@@ -1,9 +1,13 @@
+import os
+from flask import send_from_directory
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def experiment():
-    return app.send_static_file('templates/index.html')
+    root_dir = os.path.dirname(os.getcwd())
+    print os.path.join('.', 'templates')
+    return send_from_directory(os.path.join('.', 'templates'), 'index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
