3fe51a73fb32120dbb590c38e5ab48c19cfa39bf
Samuel Clay <samuel@ofbrooklyn.com>
2018-09-16 15:48:15 -0400

    Adding static files and template.

--- hw2.py
+++ hw2.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def experiment():
+    return app.send_static_file('templates/index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
