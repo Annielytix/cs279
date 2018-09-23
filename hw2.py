defccb3bb81788a994dcd200ceaa9a0d347f9082
Samuel Clay <samuel@ofbrooklyn.com>
2018-09-23 14:48:04 -0400

    Stubbing in ajax handler.

--- hw2.py
+++ hw2.py
@@ -1,12 +1,29 @@
-import os
-from flask import send_from_directory
+import uuid
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import make_response
+from flask import jsonify
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/hw2"
+mongo = PyMongo(app)
 
 @app.route("/")
 def experiment():
-    root_dir = os.path.dirname(os.getcwd())
-    return send_from_directory(os.path.join('.', 'templates'), 'index.html')
+    hw2uuid = request.cookies.get('hw2uuid')
+    response = make_response(render_template('index.html'))
+    
+    if not hw2uuid:
+        response.set_cookie('hw2uuid', unicode(uuid.uuid4()))
 
+    return response
+
+@app.route("/demographics", methods=["POST"])
+def demographics():
+    print request.form
+    return jsonify(dict(message="OK", form=request.form))
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
