d5cd90b4f9c394a776a8977954c4492589b30767
Samuel Clay <samuel@ofbrooklyn.com>
2018-11-11 17:59:51 -0500

    Moving old project to hw2 and copying in to final.

---
+++ hw2/hw2.py
@@ -0,0 +1,207 @@
+from random import randint
+import uuid
+import pytz
+import xlsxwriter
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import make_response
+from flask import jsonify
+from flask_pymongo import PyMongo
+from flask import send_file
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/hw2"
+mongo = PyMongo(app)
+
+@app.route("/")
+def experiment():
+    hw2uuid = request.cookies.get('hw2uuid')
+    response = make_response(render_template('index.html', random=randint(0, 100000)))
+    
+    if not hw2uuid:
+        response.set_cookie('hw2uuid', str(uuid.uuid4()))
+
+    return response
+
+@app.route("/demographics", methods=["POST"])
+def demographics():
+    print(" ---> Submitting demographics: %s" % request.form)
+    mongo.db.demographics.insert({
+        "uuid": request.cookies.get('hw2uuid'),
+        "gender": request.form.get("gender"),
+        "age": request.form.get("age"),
+        "education": request.form.get("education"),
+        "pointer": request.form.get("pointer"),
+        "handedness": request.form.get("handedness"),
+        "language": request.form.get("language"),
+        "experience": request.form.get("experience"),
+    })
+    return jsonify(dict(message="OK", form=request.form))
+
+@app.route("/feedback", methods=["POST"])
+def feedback():
+    print(" ---> Submitting feedback: %s" % request.form)
+    mongo.db.feedback.insert({
+        "uuid": request.cookies.get('hw2uuid'),
+        "difficulty": request.form.get("difficulty"),
+        "satisfaction": request.form.get("satisfaction"),
+        "frustration": request.form.get("frustration"),
+        "efficiency": request.form.get("efficiency"),
+    })
+    return jsonify(dict(message="OK", form=request.form))
+
+@app.route("/taskdata", methods=["POST"])
+def taskdata():
+    result = request.get_json()
+    print(" ---> Submitting task data: %s" % result)
+    mongo.db.taskdata.insert({
+        "uuid": request.cookies.get('hw2uuid'),
+        "condition": result.get('condition'),
+        "permutation": result.get("permutation"),
+        "selection": result.get("selection"),
+        "trials": result.get("trials"),
+        "fadeIns": result.get("fadeIns")
+    })
+    return jsonify(dict(message="OK", form=request.form))
+
+@app.route("/xlsx")
+def xlsx():    
+    xlsx_path = "CS279.xlsx"
+    workbook = xlsxwriter.Workbook(xlsx_path)
+    bold = workbook.add_format({'bold': 1})
+    date_format = workbook.add_format({'num_format': 'm/dd/yy h:mm:ss AM/PM'})
+    EST = pytz.timezone('US/Eastern')
+    generate_date = lambda x: x['_id'].generation_time.astimezone(EST).replace(tzinfo=None)
+    
+    demographics = workbook.add_worksheet("Demographics")
+    feedback = workbook.add_worksheet("Feedback")
+    data = workbook.add_worksheet("Data")
+    
+    db_demographics = mongo.db.demographics.find()
+    db_feedback = mongo.db.feedback.find()
+    db_data = mongo.db.taskdata.find()
+    
+    # Feedback
+    
+    row = 0
+    col = 0
+    feedback.write(row, col, "UUID", bold)
+    feedback.set_column(col, col, 15); col += 1
+    feedback.write(row, col, "Date", bold)
+    feedback.set_column(col, col, 20); col += 1
+    feedback.write(row, col, "Difficulty", bold)
+    feedback.set_column(col, col, 15); col += 1
+    feedback.write(row, col, "Efficiency", bold)
+    feedback.set_column(col, col, 15); col += 1
+    feedback.write(row, col, "Frustration", bold)
+    feedback.set_column(col, col, 15); col += 1
+    feedback.write(row, col, "Satisfaction", bold)
+    feedback.set_column(col, col, 15); col += 1
+    
+    for f in db_feedback:
+        if not f.get('difficulty', None): continue
+        print(f)
+        row += 1
+        col = 0
+        date = generate_date(f)
+        feedback.write(row, col, f['uuid']); col += 1;
+        feedback.write_datetime(row, col, date, date_format); col += 1;
+        feedback.write(row, col, f['difficulty']); col += 1;
+        feedback.write(row, col, f['efficiency']); col += 1;
+        feedback.write(row, col, f['frustration']); col += 1;
+        feedback.write(row, col, f['satisfaction']); col += 1;
+    
+    # Demographics
+    
+    row = 0
+    col = 0
+    demographics.write(row, col, "UUID", bold)
+    demographics.set_column(col, col, 15); col += 1
+    demographics.write(row, col, "Date", bold)
+    demographics.set_column(col, col, 20); col += 1
+    demographics.write(row, col, "Age", bold)
+    demographics.set_column(col, col, 15); col += 1
+    demographics.write(row, col, "Education", bold)
+    demographics.set_column(col, col, 15); col += 1
+    demographics.write(row, col, "Experience", bold)
+    demographics.set_column(col, col, 15); col += 1
+    demographics.write(row, col, "Gender", bold)
+    demographics.set_column(col, col, 15); col += 1
+    demographics.write(row, col, "Handedness", bold)
+    demographics.set_column(col, col, 15); col += 1
+    demographics.write(row, col, "Language", bold)
+    demographics.set_column(col, col, 15); col += 1
+    demographics.write(row, col, "Pointer", bold)
+    demographics.set_column(col, col, 15); col += 1
+    
+    for d in db_demographics:
+        print(d)
+        row += 1
+        col = 0
+        date = generate_date(d)
+        demographics.write(row, col, d['uuid']); col += 1;
+        demographics.write_datetime(row, col, date, date_format); col += 1;
+        demographics.write(row, col, d.get('age', '')); col += 1;
+        demographics.write(row, col, d.get('education', '')); col += 1;
+        demographics.write(row, col, d.get('experience', '')); col += 1;
+        demographics.write(row, col, d.get('gender', '')); col += 1;
+        demographics.write(row, col, d.get('handedness', '')); col += 1;
+        demographics.write(row, col, d.get('language', '')); col += 1;
+        demographics.write(row, col, d.get('pointer', '')); col += 1;
+    
+    row = 0
+    col = 0
+    data.write(row, col, "UUID", bold)
+    data.set_column(col, col, 15); col += 1
+    data.write(row, col, "Date", bold)
+    data.set_column(col, col, 20); col += 1
+    data.write(row, col, "Condition", bold)
+    data.set_column(col, col, 15); col += 1
+    data.write(row, col, "Fade in", bold)
+    data.set_column(col, col, 15); col += 1
+    data.write(row, col, "Permutation", bold)
+    data.set_column(col, col, 15); col += 1
+    data.write(row, col, "Selection", bold)
+    data.set_column(col, col, 15); col += 1
+    data.write(row, col, "Correct", bold)
+    data.set_column(col, col, 15); col += 1
+    data.write(row, col, "Menu Timestamp", bold)
+    data.set_column(col, col, 15); col += 1
+    data.write(row, col, "Menu Option Timestamp", bold)
+    data.set_column(col, col, 15); col += 1
+    
+    # Task data
+    
+    for d in db_data:
+        for t, trial in enumerate(d['trials']):
+            print(d)
+            row += 1
+            col = 0
+            date = generate_date(d)
+            data.write(row, col, d['uuid']); col += 1;
+            data.write_datetime(row, col, date, date_format); col += 1;
+            data.write(row, col, d['condition']); col += 1;
+            if d.get('fadeIns', None) and len(d['fadeIns']) > t:
+                data.write(row, col, d['fadeIns'][t]); col += 1;
+            else:
+                data.write(row, col, ""); col += 1;
+            data.write(row, col, ', '.join([str(p) for p in d['permutation']])); col += 1;
+            if d.get('selection', None) and len(d['selection']) > t:
+                data.write(row, col, d['selection'][t]); col += 1;
+            else:
+                data.write(row, col, ""); col += 1;
+            data.write(row, col, trial['correct']); col += 1;
+            data.write(row, col, trial['events'][-1]['timestamp']); col += 1;
+            data.write(row, col, trial['events'][-2]['timestamp']); col += 1;
+    
+    
+    workbook.close()
+    
+    return send_file(xlsx_path,
+              attachment_filename=xlsx_path,
+              as_attachment=True)
+    
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ hw2/static/hw2.js
@@ -0,0 +1,509 @@
+$(document).ready(function() {
+  window.HW2 = {};
+  HW2.modalOptions = {
+    'keyboard': false,
+    'backdrop': 'static'
+  };
+
+  // option groups
+  HW2.groups = [
+    ["Butterfly", "Mosquito", "Moth", "Cicada"],
+    ["Grapes", "Cranberry", "Lemon", "Apple"],
+    ["English", "French", "Chinese", "Arabic"],
+    ["Bass", "Trout", "Salmon", "Tuna"],
+    ["Sofa", "Table", "Bed", "Chair"],
+    ["Ammonia", "Chlorine", "Acetone", "Ethanol"],
+    ["Car", "Boat", "Bus", "Train"],
+    ["Celery", "Broccoli", "Cabbage", "Cauliflower"],
+    ["Merlot", "Champagne", "Cabernet", "Chardonnay"],
+    ["Oak", "Maple", "Walnut", "Mahogany"],
+    ["Padauk", "Bubinga", "Cocobolo", "Poculi"],
+    ["Sunflower", "Canola", "Sesame", "Olive"],
+    ["Cup", "Goblet", "Glass", "Mug"],
+    ["Sapphire", "Ruby", "Diamond", "Pearl"],
+    ["Toothpaste", "Floss", "Mouthwash", "Toothbrush"],
+    ["Python", "Java", "Ruby", "Go"],
+    ["Rock", "Jazz", "Pop", "Country"],
+    ["Windows", "iOS", "Linux", "MacOS"],
+    ["Ruler", "Compass", "Protractor", "Angle"],
+    ["Adidas", "Nike", "Reebok", "Puma"],
+    ["Beyonce", "Prince", "Beatles", "Katy Perry"],
+    ["Airplane", "Helicopter", "Blimp", "Rocket"],
+    ["Daffodil", "Rose", "Tulip", "Pansy"],
+    ["Rucksack", "Bag", "Backpack", "Satchel"]];
+  HW2.groups = _.shuffle(HW2.groups);
+  for (var i = 0; i < HW2.groups.length; i++) {
+    HW2.groups[i] = _.shuffle(HW2.groups[i]);
+  }
+
+  HW2.practiceGroups = [
+    ["Starfruit", "Durian", "Pomegranate", "Passionfruit"],
+    ["Alphonso", "Chaunsa", "Fazli", "Raspuri"],
+    ["Flounder", "Bream", "Haddock", "Mackerel"],
+    ["Sicilian", "Chicago", "New York", "Thin Crust"],
+    ["Tree", "Flower", "Bamboo", "Seaweed"],
+    ["MongoDB", "Postgres", "MonetDB", "Firebase"],
+    ["Fir", "Alder", "Cedar", "Poplar"],
+    ["Tomato", "Eggplant", "Cucumber", "Squash"],
+    ["Drawer", "Shelves", "Wardrobe", "Desk"],
+    ["Mewtwo", "Ghastly", "Meowth", "Pikachu"],
+    ["Mouse", "Keyboard", "Monitor", "Webcam"],
+    ["Hamster", "Gerbil", "Chinchilla", "Guinea Pig"]];
+  HW2.practiceGroups = _.shuffle(HW2.practiceGroups);
+  for (var i = 0; i < HW2.practiceGroups.length; i++) {
+    HW2.practiceGroups[i] = _.shuffle(HW2.practiceGroups[i]);
+  }
+
+  // set up experimental params
+  HW2.experimentParams = {};
+
+  // selections: 8 items per menu, weighted as 15, 8, 5, 4, 3, 3, 2, 2 (= 42) ("Zipfian")
+  var EASY_MODE = false;
+  var copies = EASY_MODE ? [1, 1] : [15, 8, 5, 4, 3, 3, 2, 2];
+  var selections = [];
+  for (var m = 0; m < 3; m++) {
+    var picked = _.shuffle(_.range(16));
+    for (var i = 0; i < copies.length; i++) {
+      selections = selections.concat(Array(copies[i]).fill(picked[i] + m * 16));
+    }
+  }
+  HW2.experimentParams.selections = _.shuffle(selections);
+  HW2.experimentParams.conditions = _.shuffle(["Baseline", "Ephemeral"]);
+
+  HW2.begin = function() {
+    if (window.location.hash.indexOf('skip') != -1) {
+      HW2.preparePracticeExperiment();
+    } else if (window.location.hash.indexOf('baseline') != -1) {
+      HW2.experimentParams.conditions = ["Baseline"];
+      HW2.finishPracticeExperiment();
+    } else if (window.location.hash.indexOf('ephemeral') != -1) {
+      HW2.experimentParams.conditions = ["Ephemeral"];
+      HW2.finishPracticeExperiment();
+    } else if (window.location.hash.indexOf('survey') != -1) {
+      HW2.finishMainExperiment(null);
+    } else {
+      $('#begin-modal').modal(HW2.modalOptions);
+      $('#submit-begin').on("click", function() {
+        HW2.prepareDemographicsForm();
+      });
+    }
+  };
+  
+  HW2.prepareDemographicsForm = function() {
+    $('#begin-modal').modal('hide');
+    $('#demographics-modal').modal(HW2.modalOptions);
+    $("#demographics-submit").on("click", function() {
+      HW2.submitDemographicsForm();
+    });
+  };
+  
+  HW2.submitDemographicsForm = function() {
+    $("#demographics-modal").modal('hide');
+
+    $.ajax({
+      'type': "POST",
+      'url': '/demographics',
+      'data': {
+        'gender': $("input[name=gender]").val(),
+        'age': $("input[name=age]").val(),
+        'education': $("select[name=education]").val(),
+        'pointer': $("select[name=pointer]").val(),
+        'handedness': $("select[name=handedness]").val(),
+        'language': $("input[name=language]").val(),
+        'experience': $("select[name=experience]").val()
+      },
+      'success': function() {
+        HW2.preparePracticeModal();
+      }
+    });
+  };
+
+  HW2.preparePracticeModal = function() {
+    $("#practice-modal").modal(HW2.modalOptions);
+
+    $("#submit-begin-experiment").off(".begin").on("click.begin", function() {
+      HW2.preparePracticeExperiment();
+    });
+  };
+
+  HW2.preparePracticeExperiment = function() {
+    console.log("preparePracticeExperiment");
+    $("#practice-modal").modal('hide');
+    $("#intermission-modal").modal('hide');
+    $("#feedback-modal").modal('hide');
+
+    var fadeIn;
+    if (HW2.experimentParams.conditions[0] === "Baseline") {
+      fadeIn = false;
+    } else {
+      fadeIn = true;
+    }
+
+    // construct experiment object
+    var items = _.flatten(HW2.practiceGroups);
+    var trials = _.shuffle(_.range(48)).slice(0, EASY_MODE ? 2 : 8); // just pick 8 menu items
+
+    var exp = new HW2.Experiment(items, trials, fadeIn, HW2.finishPracticeExperiment);
+    exp.install();
+
+    $("#experiment").show();
+  };
+
+  HW2.finishPracticeExperiment = function(exp) {
+    console.log("finishPracticeExperiment");
+    $("#experiment").hide();
+
+    if (HW2.experimentParams.conditions.length === 1) {
+      $("#intermission-modal-title").text("Almost finished!");
+    }
+    $("#intermission-modal").modal(HW2.modalOptions);
+
+    $("#submit-intermission").off(".intermission").on("click.intermission", function() {
+      HW2.prepareMainExperiment();
+    });
+  };
+
+  HW2.prepareMainExperiment = function() {
+    console.log("prepareMainExperiment");
+    $("#intermission-modal").modal('hide');
+
+    var fadeIn;
+    if (HW2.experimentParams.conditions[0] === "Baseline") {
+      fadeIn = false;
+    } else {
+      fadeIn = true;
+    }
+
+    // construct experiment object
+    var items = _.flatten(HW2.groups.slice(0, 12));
+    HW2.groups = HW2.groups.slice(12); // jankily cut off the used groups
+    var trials = HW2.experimentParams.selections.slice(0);
+
+    var exp = new HW2.Experiment(items, trials, fadeIn, HW2.finishMainExperiment);
+    exp.install();
+
+    $("#experiment").show();
+  };
+  
+  HW2.finishMainExperiment = function(exp) {
+    console.log("finishMainExperiment");
+    console.log(exp);
+
+    // send the experimental results off to the server
+    if(exp){
+      $.ajax({
+        'type': "POST",
+        'url': '/taskdata',
+        'contentType': "application/json; charset=utf-8",
+        'dataType': "json",
+        'data': JSON.stringify( exp )
+      });
+    }
+
+    $("#experiment").hide();
+    $("#feedback-modal").modal(HW2.modalOptions);
+
+    $("#submit-feedback").off(".feedback").on("click.feedback", function() {
+      $.ajax({
+        'type': "POST",
+        'url': '/feedback',
+        'data': {
+          'difficulty': $("input[name=difficulty]:checked").val(),
+          'satisfaction': $("input[name=satisfaction]:checked").val(),
+          'frustration': $("input[name=frustration]:checked").val(),
+          'efficiency': $("input[name=efficiency]:checked").val()
+        },
+        'success': function() {
+          // clear radio buttons
+          console.log("CLEARING");
+          $("input[name=difficulty]:checked").prop("checked", false);
+          $("input[name=satisfaction]:checked").prop("checked", false);
+          $("input[name=frustration]:checked").prop("checked", false);
+          $("input[name=efficiency]:checked").prop("checked", false);
+
+          // show next experiment if a condition remains
+          HW2.experimentParams.conditions = HW2.experimentParams.conditions.slice(1);
+          if (HW2.experimentParams.conditions.length === 0)
+            HW2.prepareGoodbye();
+          else
+            HW2.preparePracticeExperiment();
+        }
+      });
+    });
+  };
+
+  HW2.prepareGoodbye = function() {
+    $("#feedback-modal").modal("hide");
+    $("#goodbye-modal").modal(HW2.modalOptions);
+  };
+
+  /* expected arguments:
+   * items - a flat list of 48 items representing 12 semantically related groups of 4
+   * selections - order to prompt item selection (will be obfuscated by menu permutation)
+   * fadeIn - experimental condition
+   * finishHook - hook to call when done with experiment */
+  HW2.Experiment = function(items, selections, fadeIn, finishHook) {
+    var mkMenu = function(menuNum, dropdown) {
+      return _.template('\
+        <div class="experiment-menu">\
+        <div id="experiment-menu-button-<%= menuNum %>" class="experiment-menu-button">Menu <%= menuNum %></div>\
+        <%= dropdown %>\
+        </div>')({menuNum: menuNum, dropdown: dropdown});
+    };
+
+    var mkMenuDropdown = function(menuNum, options) {
+      return _.template('\
+        <div id="experiment-menu-dropdown-<%= menuNum %>" class="experiment-menu-dropdown" style="display:none;">\
+        <%= options %>\
+        </div>')({menuNum: menuNum, options: options});
+    };
+
+    var mkMenuOption = function(menuNum, optionNum, text) {
+      return _.template('\
+        <div id="experiment-menu-<%= menuNum %>-option-<%= optionNum %>" class="experiment-menu-option">\
+        <span class="menu-option-text"><%= text %></span>\
+        </div>')({menuNum: menuNum, optionNum: optionNum, text: text});
+    };
+
+    // construct menu HTML
+    var menuElements = "";
+    var menuItems = items.slice(0);
+    this.menuItems = []; // save item order
+    for (var m = 1; m <= 3; m++) {
+      var options = "";
+      for (var o = 1; o <= 16; o++) {
+        var nextItem = menuItems.pop();
+        this.menuItems.push(nextItem);
+        options += mkMenuOption(m, o, nextItem);
+      }
+
+      menuElements += mkMenu(m, mkMenuDropdown(m, options));
+    }
+    this.menuElements = menuElements;
+
+    // ****************************
+    // * SELECTION HANDLING LOGIC *
+    // ****************************
+
+    // pre-select 80% of trials for correct fade-ins
+    var numCorrectTrials = Math.floor(selections.length * 0.8);
+    var numIncorrectTrials = selections.length - numCorrectTrials;
+    this.fadeCorrect = _.shuffle(Array(numCorrectTrials).fill(true)
+      .concat(Array(numIncorrectTrials).fill(false)));
+
+    this.fadeDecided = this.fadeCorrect.slice(0);
+
+    // pick a random menu permutation
+    var menuPerm = _.shuffle(_.range(3));
+
+    // get [menu, opt] intended by current selection
+    this.selections = selections.slice(0);
+    this.getSelection = function() {
+      var menu = menuPerm[Math.floor(this.selections[0] / 16)];
+      var opt = this.selections[0] % 16;
+
+      return [menu, opt];
+    };
+
+    this.showTrial = function() {
+      var loc = this.getSelection();
+
+      // set prompt
+      var promptText = _.template('Menu <%= loc %> > <%= menuItems %>')({
+        loc: loc[0] + 1,
+        menuItems: this.menuItems[loc[0]*16 + loc[1]]
+      });
+      $("#experiment-prompt").text(promptText);
+
+      var menuOptionId = function(m, o) {
+        return _.template('#experiment-menu-<%= menu %>-option-<%= opt %> .menu-option-text')({
+          menu: m,
+          opt: o
+        });
+      };
+
+      // set fade-ins
+      if (fadeIn) {
+        $(".experiment-menu-option .menu-option-text").addClass("fade-in");
+
+        if (this.fadeCorrect[0]) {
+          // unfade correct item
+          $(menuOptionId(loc[0]+1, loc[1]+1)).removeClass("fade-in");
+
+          // unfade two incorrect in same menu
+          var wrong = _.shuffle(_.range(16));
+          var unfaded = 0;
+          for (var o = 0; unfaded < 2; o++) {
+            if (wrong[o] !== loc[1]) {
+              $(menuOptionId(loc[0]+1, wrong[o]+1)).removeClass("fade-in");
+              unfaded++;
+            }
+          }
+          
+          // unfade three in each other menu
+          for (var m = 0; m < 3; m++) {
+            if (m !== loc[0]) {
+              var wrong = _.shuffle(_.range(16));
+              for (var o = 0; o < 3; o++) {
+                $(menuOptionId(m+1, wrong[o]+1)).removeClass("fade-in");
+              }
+            }
+          }
+        } else {
+          // select three incorrect items per menu
+          for (var m = 0; m < 3; m++) {
+            var wrong = _.shuffle(_.range(16));
+            var unfaded = 0;
+            for (var o = 0; unfaded < 3; o++) {
+              if (m !== loc[0] || wrong[o] !== loc[1]) {
+                $(menuOptionId(m+1, wrong[o]+1)).removeClass("fade-in");
+                unfaded++;
+              }
+            }
+          }
+        }
+      }
+    };
+
+    this.nextTrial = function() {
+      this.finishTrial();
+
+      this.selections = this.selections.slice(1);
+      this.fadeCorrect = this.fadeCorrect.slice(1);
+
+      if (this.selections.length === 0) {
+        // pack up the measurements and pass out to the hook
+        var exp = {
+          "condition": fadeIn ? "Ephemeral" : "Baseline",
+          "permutation": menuPerm,
+          "selection": selections,
+          "trials": this.trials,
+          "fadeIns": this.fadeDecided
+        };
+
+        finishHook(exp);
+      } else {
+        this.showTrial();
+      }
+    }
+
+    // **********************
+    // * TRIAL DATA LOGGING *
+    // **********************
+
+    this.trials = [];
+    this.curTrial = {
+      "correct" : true,
+      "expected" : this.getSelection(),
+      "events" : []
+    };
+
+    this.finishTrial = function () {
+      this.trials.push(this.curTrial);
+      this.curTrial = {
+        "correct" : true,
+        "expected" : this.getSelection(),
+        "events" : []
+      };
+    };
+
+    this.trialSetIncorrect = function () {
+      this.curTrial["correct"] = false;
+    };
+
+    this.trialLogMenuButtonEvent = function(menuNum, timestamp) {
+      var e = {
+        "type" : "button",
+        "timestamp" : timestamp,
+        "menuNum" : menuNum
+      };
+
+      this.curTrial.events.push(e);
+    };
+
+    this.trialLogMenuOptionEvent = function(menuNum, optionNum, timestamp) {
+      var e = {
+        "type" : "option",
+        "timestamp" : timestamp,
+        "menuNum" : menuNum,
+        "optionNum" : optionNum
+      };
+
+      this.curTrial.events.push(e);
+    };
+
+    // ******************************
+    // * DISPLAY AND EVENT HANDLING *
+    // ******************************
+
+    // handle menu clicks
+    this.menuButtonClicked = function(menuNum, timestamp) {
+      this.trialLogMenuButtonEvent(menuNum, timestamp);
+    };
+
+    // handle option clicks
+    this.menuOptionClicked = function(menuNum, optionNum, timestamp) {
+      this.trialLogMenuOptionEvent(menuNum, optionNum, timestamp);
+
+      // advance to next trial if appropriate
+      var loc = this.getSelection();
+      if (menuNum - 1 === loc[0] && optionNum - 1 === loc[1]) {
+        this.nextTrial();
+      } else {
+        this.trialSetIncorrect();
+      }
+    };
+
+    this.install = function() {
+      console.log("NOW INSTALLING NEW EXPERIMENT");
+
+      var instance = this;
+
+      // clear current menus, drop constructed menus
+      $(".experiment-menubar").empty();
+      $(".experiment-menubar").append(this.menuElements);
+
+      // update prompt text and menu fade-ins
+      this.showTrial();
+
+      // install measurement handlers
+      $(".experiment-menu-button").on("click", function (e) {
+        m = /experiment-menu-button-(\d+)/.exec($(this).attr("id"));
+        // XXX timeStamp is NOT supported in Firefox
+        // XXX timeStamp in Chrome is time since page loaded
+        instance.menuButtonClicked(m[1], e.timeStamp);
+      });
+      $(".experiment-menu-option").on("click", function (e) {
+        m = /experiment-menu-(\d+)-option-(\d+)/.exec($(this).attr("id"));
+        instance.menuOptionClicked(m[1], m[2], e.timeStamp);
+      });
+
+      // menu show/hide functionality
+      $("#experiment-menu-button-1").on("click", function() {
+        $("#experiment-menu-dropdown-1").toggle();
+        $("#experiment-menu-dropdown-2").hide();
+        $("#experiment-menu-dropdown-3").hide();
+        $(".fade-in").hide().fadeIn(500);
+      });
+      $("#experiment-menu-button-2").on("click", function() {
+        $("#experiment-menu-dropdown-1").hide();
+        $("#experiment-menu-dropdown-2").toggle();
+        $("#experiment-menu-dropdown-3").hide();
+        $(".fade-in").hide().fadeIn(500);
+      });
+      $("#experiment-menu-button-3").on("click", function() {
+        $("#experiment-menu-dropdown-1").hide();
+        $("#experiment-menu-dropdown-2").hide();
+        $("#experiment-menu-dropdown-3").toggle();
+        $(".fade-in").hide().fadeIn(500);
+      });
+      $(".experiment-menu-option").on("click", function () {
+        $("#experiment-menu-dropdown-1").hide();
+        $("#experiment-menu-dropdown-2").hide();
+        $("#experiment-menu-dropdown-3").hide();
+      });
+    };
+  };
+
+  HW2.begin();
+});
